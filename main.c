513f2ae4e872123f00a25a5cb2a1a927e269b3a2
JJK08 <jinjiakai2008@gmail.com>
2024-09-25 15:21:58 +0200

    3.1 pari dispa

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%2==0) {
+        printf("%d e' pari\n:",a);
+    }else {
+        printf("%d e' dispri\n: ",a);
+    }
+    return 0;
+}
